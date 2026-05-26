a5f5a2da7cea225d09903fc9fc1ad8242d659ae9
LaetitiaBraun <laetitia.pianiste@gmail.com>
2026-05-26 14:04:53 +0200

    Create routerController.php

---
+++ controller/routerController.php
@@ -0,0 +1,21 @@
+<?php
+# controller/routerController.php
+
+require PROJECT_PATH."/model/guestbookModel.php";
+
+try{
+    $connectDB = new PDO(DB_DSN, DB_LOGIN, DB_PWD);
+}catch(Exception $e){
+    die($e->getMessage());
+}
+
+if(isset($_POST['usermail'], $_POST['message'])){
+    $insert = insertMessage($connectDB, $_POST['usermail'], $_POST['message']);
+}
+
+$messages = selectAllMessage($connectDB);
+
+$connectDB = null;
+
+include PROJECT_PATH."/view/guestbookView.php";
+?>
